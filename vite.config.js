d3ff7267aaf1ab0297c2a38892b59230e02d537e
Christian Mendoza <chimen0531@gmail.com>
2026-08-27 11:10:18 +0800

    Update vite.config.js for PWA

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,42 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      workbox: {
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+    }),
+  ],
+
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['privacy-diameter-reseal.ngrok-free.dev'],   // <-- add this line
+
+    allowedHosts: ['privacy-diameter-reseal.ngrok-free.dev'],
+
     proxy: {
       '/api': 'http://127.0.0.1:8000',
       '/auth': 'http://127.0.0.1:8000',
       '/admin': 'http://127.0.0.1:8000',
       '/uploads': 'http://127.0.0.1:8000',
       '/chat': 'http://127.0.0.1:8000',
-    }
+    },
   },
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
